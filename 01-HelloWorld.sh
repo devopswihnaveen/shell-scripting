0ce87d73f43f0c2be6411fe19027196fadaa6bbb
naveen <naveenkumarvelanati@gmail.com>
2026-01-28 07:57:25 +0530

    Hello world shell script

---
+++ 01-HelloWorld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# A simple Hello World script
+echo "Hello, World!"
+echo "This is my first shell script after long time."
+echo "Have a great day!"
+# End of script
+# Thank you for running this script.
+# This script was created for demonstration purposes.
+# Feel free to modify and use it as you like.
+# Remember to give execute permission using chmod +x 01-HelloWorld.sh
+# Run the script using ./01-HelloWorld.sh
+# Enjoy coding!
+# Stay safe and happy coding!# See you next time!
+# Keep learning and exploring new things!
+# Cheers!
+# This is the end of the script.
+# Goodbye!
